f7ac17d6b937011b30524b3f064f3cb27c35b9e4
artemka1511 <artemka15111992@gmail.com>
2023-03-27 00:38:53 +0700

    first commit

---
+++ FindWords/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FindWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("Text1.txt");
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            var words = noPunctuationText.Split(new char[] { ' ' });
+
+            var dictWords = new Dictionary<string, int>();
+
+            foreach (var item in words)
+            {
+                if (!dictWords.ContainsKey(item))
+                {
+                    dictWords.Add(item, 0);
+                }
+                else
+                {
+                    dictWords[item] += 1;
+                }
+            }
+
+            dictWords.Remove(""); // Костыль :)
+
+            dictWords = dictWords.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (KeyValuePair<string, int> kvp in dictWords.Take(10))
+            {
+                Console.WriteLine($"Слово: {kvp.Key}. Количество раз: {kvp.Value}");
+            }
+        }
+    }
+}
